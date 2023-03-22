934ad1ebcba33680513f4c1519d85589e839edfa
Rodrigo-RRD <116466164+Rodrigo-RRD@users.noreply.github.com>
2023-03-21 22:46:59 -0300

    Add codigo exemplo para com o jar

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("By rodrigo rodrigues ");
+        System.out.println("\n──────▄▀▄─────▄▀▄\n" +
+                "─────▄█░░▀▀▀▀▀░░█▄\n" +
+                "─▄▄──█░░░░░░░░░░░█──▄▄\n" +
+                "█▄▄█─█░░▀░░┬░░▀░░█─█▄▄█");
+        System.out.println("      Com sucesso!");
+        Scanner sc = new Scanner(System.in);
+        sc.next();
+    }
+}
