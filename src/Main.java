e15785bc7fcaa9c1bfe32881f50cdafc956f1fcb
Rodrigo-RRD <116466164+Rodrigo-RRD@users.noreply.github.com>
2023-03-22 00:45:12 -0300

    Add gitignore com novos tipos - .jar e .bat

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("By rodrigo rodrigues ");
+        System.out.println("By Rodrigo Rodrigues and Michael Marinho ");
         System.out.println("\n──────▄▀▄─────▄▀▄\n" +
                 "─────▄█░░▀▀▀▀▀░░█▄\n" +
                 "─▄▄──█░░░░░░░░░░░█──▄▄\n" +
